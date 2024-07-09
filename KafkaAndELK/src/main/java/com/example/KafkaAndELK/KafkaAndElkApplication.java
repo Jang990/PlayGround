87c81d9fc2db302471763d95b1da7f1f7444d8df
Jang <sdsd090811@gmail.com>
2024-07-09 18:03:46 +0900

    feat: Kafka & ELK - 프로젝트 추가

---
+++ KafkaAndELK/src/main/java/com/example/KafkaAndELK/KafkaAndElkApplication.java
@@ -0,0 +1,13 @@
+package com.example.KafkaAndELK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAndElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAndElkApplication.class, args);
+	}
+
+}
---
+++ KafkaAndELK/src/test/java/com/example/KafkaAndELK/KafkaAndElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KafkaAndELK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAndElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
