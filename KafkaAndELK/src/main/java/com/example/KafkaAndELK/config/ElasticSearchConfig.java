1019ccfd49494effa85d9e17ac4b952600f0f99d
Jang <sdsd090811@gmail.com>
2024-07-11 18:52:54 +0900

    feat: Kafka & ELK - docker-compose es 추가

---
+++ KafkaAndELK/src/main/java/com/example/KafkaAndELK/config/ElasticSearchConfig.java
@@ -0,0 +1,24 @@
+package com.example.KafkaAndELK.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+    @Value("${elastic.search.uri}")
+    private String uri;
+    @Value("${elastic.search.username}")
+    private String username;
+    @Value("${elastic.search.password}")
+    private String password;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(uri)
+                .withBasicAuth(username, password)
+                .build();
+    }
+}
