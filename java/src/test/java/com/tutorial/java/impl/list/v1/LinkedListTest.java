92fd958a7a21ed95b1189a62acda6e9d1de96213
Jang <sdsd090811@gmail.com>
2024-08-18 14:58:33 +0900

    feat: 자바 - LinkedList 테스트 코드 추가

---
+++ java/src/test/java/com/tutorial/java/impl/list/v1/LinkedListTest.java
@@ -0,0 +1,72 @@
+package com.tutorial.java.impl.list.v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LinkedListTest {
+    private LinkedList<Integer> list;
+    private final int[] INSTANCE = {236, 10012, 5465};
+    final String[] STRING_INSTANCE = {"A", "B", "C"};
+
+    @BeforeEach
+    void beforeEach() {
+        list = new LinkedList<Integer>();
+        list.add(INSTANCE[0]);
+        list.add(INSTANCE[1]);
+        list.add(INSTANCE[2]);
+    }
+
+    @Test
+    void get() {
+        assertThat(list.get(0)).isEqualTo(INSTANCE[0]);
+        assertThat(list.get(1)).isEqualTo(INSTANCE[1]);
+        assertThat(list.get(2)).isEqualTo(INSTANCE[2]);
+    }
+
+    @Test
+    void testToString() {
+        System.out.println(list);
+        assertThat(list.toString()).isEqualTo("["+INSTANCE[0]+", "+INSTANCE[1]+", "+INSTANCE[2]+"]");
+    }
+
+    @Test
+    void size() {
+        assertThat(list.size()).isEqualTo(INSTANCE.length);
+    }
+
+    @Test
+    void insert() {
+        int insertedNum = -1;
+        int insertedIdx = 1;
+        list.add(insertedIdx, insertedNum);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (insertedIdx == i) {
+                assertThat(list.get(i)).isEqualTo(insertedNum);
+                continue;
+            }
+            if (insertedIdx < i) {
+                assertThat(list.get(i)).isEqualTo(INSTANCE[i - 1]);
+                continue;
+            }
+
+            assertThat(list.get(i)).isEqualTo(INSTANCE[i]);
+        }
+    }
+
+    @Test
+    void stringList() {
+        MyList<String> myStringList = new MyList<>();
+        for (int i = 0; i < STRING_INSTANCE.length; i++) {
+            myStringList.add(STRING_INSTANCE[i]);
+        }
+
+        for (int i = 0; i < myStringList.size(); i++) {
+            assertThat(myStringList.get(i)).isEqualTo(STRING_INSTANCE[i]);
+        }
+    }
+}
